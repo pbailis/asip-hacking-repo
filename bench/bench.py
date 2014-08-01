f9029b76f3b93746cbef05e43dde7e1bac529738
Peter Bailis <pbailis@cs.berkeley.edu>
2014-08-01 13:42:29 -0700

    Updating benchmark scripts; lightly tested and working.

--- bench/bench.py
+++ bench/bench.py
@@ -3,6 +3,13 @@ from os import system
 
 ALGORITHMS = ["LR", "SVM", "SVMADMM", "SVMADMMAsync"]
 
+class Result:
+    def __init__(self, algorithm, runtime_ms, area_under_pr, training_loss):
+        self.algorithm = algorithm
+        self.runtime_ms = runtime_ms
+        self.area_under_pr = area_under_pr
+        self.training_loss = training_loss
+
 def describe_point_cloud(pointsPerPartition = 10000,
                          partitionSkew = 0,
                          labelNoise = 0.,
@@ -26,20 +33,19 @@ def make_run_cmd(algorithm,
                  iterationEnd = 12,
                  iterationStep = 2,
                  miscStr = ""):
-    return "cd spark; sbin/stop-all.sh; sleep 5; sbin/start-all.sh;" \
+    return "cd ~/spark; sbin/stop-all.sh; sleep 5; sbin/start-all.sh;" \
            "./bin/spark-submit " \
            "--class org.apache.spark.examples.mllib.research.SynchronousADMMTests" \
            " examples/target/scala-*/spark-examples-*.jar " \
            "--algorithm %s " \
            "--regType %s " \
            "--regParam %f " \
-           "--format %s |" \
+           "--format %s " \
            "--numPartitions %d " \
            "--sweepIterationStart %d " \
            "--sweepIterationEnd %d " \
            " --sweepIterationStep %d " \
-           " %s %s " \
-           " | grep RESULT | cut -c 9-" % \
+           " %s %s " % \
             (algorithm,
              regType,
              regParam,
@@ -51,11 +57,25 @@ def make_run_cmd(algorithm,
              datasetConfigStr,
              miscStr)
 
-def runTest(cmd):
-    system("%s 2>&1 > /tmp/run.out" % (cmd))
+def runTest(algorithm, cmd):
+    print cmd
+    system("eval '%s' > /tmp/run.out 2>&1" % (cmd))
+
+    results = []
 
     for line in open("/tmp/run.out"):
-        print line
+        if line.find("RESULT") != -1:
+            line = line.split()
+            runtime_ms = int(line[2])
+            area_under_pr = float(line[3])
+            training_loss = float(line[4])
+            results.append(Result(algorithm, runtime_ms, area_under_pr, training_loss))
 
-for alg in ALGORITHMS:
-    runCmd = make_run_cmd(alg, "cloud", describe_point_cloud())
+    return results
+
+for algorithm in ALGORITHMS:
+    results = []
+    results += runTest(algorithm, make_run_cmd(algorithm, "cloud", describe_point_cloud()))
+    for r in results:
+        print r.algorithm, r.runtime_ms, r.area_under_pr
+    
