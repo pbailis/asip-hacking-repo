b6708bda0e058845edd478cb3d218d039ac41e7b
Joseph Gonzalez <jegonzal@eecs.berkeley.edu>
2014-08-11 17:33:02 +0000

    finished round of experiments

--- printexp.py
+++ printexp.py
@@ -9,8 +9,10 @@ print "StatsKeys: ", results[0]['stats'].keys()
 tbl = []
 
 for r in results:
-    tbl += [(r['pyConfig']['dataset'], r['algorithm'], r['runtimeMS'], r['iterations'], r['totalLoss'],  r['trainingError'], r['roc'], r['stats']['avgSGDIters'])]
+    tbl += [(r['pyConfig']['dataset'], r['algorithm'], r['runtimeMS'], r['iterations'], r['totalLoss'],  r['trainingError'], r['roc'])] #, r['stats']['avgSGDIters'])]
 #    print r
 
-frame = pd.DataFrame.from_records(tbl, columns=['Data', 'Alg', 'Runtime', 'Iterations', 'Loss', 'Error', 'Roc', 'SGDItres'])
-print frame
+#print tbl
+
+frame = pd.DataFrame.from_records(tbl, columns=['Data', 'Alg', 'Runtime', 'Iterations', 'Loss', 'Error', 'Roc']) #, 'SGDItres'])
+print frame.to_string()
