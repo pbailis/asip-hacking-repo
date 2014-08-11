46d664d25cf6a011e34626d62a701389b5203309
Joseph Gonzalez <jegonzal@eecs.berkeley.edu>
2014-08-11 04:03:37 +0000

    improving printout

--- printexp.py
+++ printexp.py
@@ -2,4 +2,4 @@
 import pickle
 
 for r in pickle.load(open("experiment.pkl")):
-    print r['pyConfig']['dataset'], r['algorithm'], r['runtimeMS'], r['iterations'], r['totalLoss'], r['pr'], r['roc']
+    print r['pyConfig']['dataset'], r['algorithm'][:15].ljust(15),  r['runtimeMS'], ": \t", r['iterations'], "\t", r['totalLoss'], "\t", r['trainingError'], "\t", r['roc']
