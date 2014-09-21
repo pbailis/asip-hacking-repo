0842d6143fb0cbeeb0c7e90386b25bb6435fa766
Peter Bailis <pbailis@cs.berkeley.edu>
2014-09-20 19:06:40 -0700

    update plotting scripts

--- bench/graph/plotall.py
+++ bench/graph/plotall.py
@@ -5,8 +5,10 @@ from sys import argv
 import glob
 
 logLoss = False
-yval = 'totalLoss'
+yval = 'objective'
 matplotlib.rcParams['figure.figsize'] = 10, 7#3.5, 1.7
+
+fnRegCombos = [("SVM", "L2"), ("SVM", "L1"), ("LR", "L2"), ("LR", "L1")]
 
 if len(argv) < 2:
     pickle_filename = "08_08_14_overnight.pkl"
@@ -22,6 +24,7 @@ for r in glob.glob(pickle_filename):
 #results = [r for r in results if r['algorithm'] != "HOGWILD"]
 
 print results[0].keys()
+print results[0]['pyConfig'].keys()
 
 # detect legacy data and update fields
 if "runtime_ms" not in results[0].keys():
@@ -33,41 +36,49 @@ if "runtime_ms" not in results[0].keys():
         r['dataset'] = r['pyConfig']['dataset']
         r['pointCloudDim'] = r['pyConfig']['pointCloudDim']
         r['pointCloudSkew'] = r['pyConfig']['pointCloudSkew']
-        r['total_loss'] = r['totalLoss']
-
+        r['total_loss'] = r['loss']
+        r['objective'] = r['objective']
+        r['objFn'] = r['pyConfig']['objective']
+        r['regType'] = r['pyConfig']['regType']
+    
 
 
 datasets = unique([r['dataset'] for r in results if r['dataset'] != 'cloud'])
 
 print datasets
 
-for dataset in datasets:
-    dataset_results = [r for r in results if r['dataset'] == dataset]
-    algs = unique([r['algorithm'] for r in dataset_results])
-    print "Dataset: " , dataset
-    for alg in algs:
-        alg_results = [r for r in dataset_results if r['algorithm'] == alg]
-        plot_p = [(r['runtime_ms'], r[yval]) for r in alg_results]
-        plot_p.sort(key = lambda x: x[0])
-        plotx = [r[0] for r in plot_p]
-        ploty = [r[1] for r in plot_p]
+for (objFn, regType) in fnRegCombos:
+    print objFn, regType
+    for dataset in datasets:
+        dataset_results = [r for r in results if r['dataset'] == dataset and r['objFn'] == objFn and r['regType'] == regType]
+        algs = unique([r['algorithm'] for r in dataset_results])
+        print "Dataset: %s %s %s" % (dataset, objFn, regType)
+        for alg in algs:
+            alg_results = [r for r in dataset_results if r['algorithm'] == alg]
+            plot_p = [(r['runtime_ms'], r[yval]) for r in alg_results]
+            plot_p.sort(key = lambda x: x[0])
+            plotx = [r[0] for r in plot_p]
+            ploty = [r[1] for r in plot_p]
 
-        for p in plot_p:
-            print "\t", alg, p[0], p[1]
+            for p in plot_p:
+                print "\t", alg, p[0], p[1]
     
-        plot(plotx, ploty, 'o-', label=alg)
+            plot(plotx, ploty, 'o-', label=alg)
 
-    if logLoss:
-        gca().set_yscale('log')
-        #gca().set_xscale('log')
+        if logLoss:
+            gca().set_yscale('log')
+            #gca().set_xscale('log')
 
-    if dataset == "wikipedia":
-        ylim(ymax=390000, ymin=175000)
+        if dataset == "wikipedia":
+            ylim(ymax=390000, ymin=175000)
 
-    legend()
-    savefig(dataset + ".pdf")
-    cla()
-    clf()
+        legend()
+        xlabel("Time (ms)")
+        ylabel("Objective")
+        title("%s-%s-%s" % (dataset, objFn, regType))
+        savefig("%s-%s-%s.png" % (dataset, objFn, regType))
+        cla()
+        clf()
 
 # POINT CLOUD PLOTS
 
